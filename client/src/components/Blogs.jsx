7697075933dcfdb43b64e6951b84d9f88b931537
Het Prajapati <het.p4@ahduni.edu.in>
2025-04-04 13:48:52 +0530

    Update Blogs component styling

--- client/src/components/Blogs.jsx
+++ client/src/components/Blogs.jsx
@@ -37,15 +37,15 @@ function Blogs() {
                 {blogs.map((blog, index) => (
                     <div
                         key={index}
-                        className="flex items-center space-x-3 cursor-pointer border-b border-[#D8CFD0] pb-2 last:border-0"
+                        className="flex items-center space-x-3 cursor-pointer border-b border-[#102647] pb-2 last:border-0"
                         onClick={() => handleBlogClick(blog)}
                     >
                         <div className="w-8 h-8 rounded-full bg-gray-300 overflow-hidden">
                             <img src="/person.png" alt="Author" className="w-full h-full object-cover" />
                         </div>
                         <div>
-                            <h3 className="text-[#D8CFD0] text-2xl font-bold">{blog.title}</h3>
-                            <p className="text-xl font-medium text-[#D8CFD0]">{blog.author.name} • {blog.date}</p>
+                            <h3 className="text-[#102647] text-2xl font-bold">{blog.title}</h3>
+                            <p className="text-lg font-medium text-dark">{blog.author.name} • {blog.date}</p>
                         </div>
                     </div>
                 ))}
@@ -54,15 +54,15 @@ function Blogs() {
             {/* Blog Details Modal */}
             {selectedBlog && (
                 <Modal isOpen={!!selectedBlog} onClose={() => setSelectedBlog(null)}>
-                    <h2 className="text-3xl flex justify-center text-[#D8CFD0] font-bold mb-4">{selectedBlog.title}</h2>
+                    <h2 className="text-3xl flex justify-center text-[#102647] font-bold mt-3 mb-4">{selectedBlog.title}</h2>
                     <div className="space-y-4">
-                        <p className="text-xl text-blue-300 font-bold mb-10">
+                        <p className="text-xl text-dark font-bold mb-10">
                             {selectedBlog.author.name} • {selectedBlog.date}
                         </p>
                         <p className="text-xl text-[#D8CFD0]">{selectedBlog.content}</p>
-                        <div className="flex justify-center space-x-2 mt-20">
+                        <div className="flex justify-center space-x-2 mt-10">
                             <button
-                                className="bg-gray-300 text-gray-700 text-xl font-bold px-4 py-2 rounded-md"
+                                className="bg-gray-300 text-gray-700 text-xl font-bold px-4 py-2 rounded-md hover:bg-gray-100"
                                 onClick={() => setSelectedBlog(null)}
                             >
                                 Close
