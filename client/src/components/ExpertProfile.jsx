cc768fcbe6385962f4d5c7da92a3f10aa7955ed9
Het Prajapati <het.p4@ahduni.edu.in>
2025-04-08 13:38:11 +0530

    Refactor Expert Profile page

--- client/src/components/ExpertProfile.jsx
+++ client/src/components/ExpertProfile.jsx
@@ -62,6 +62,7 @@ function ExpertProfile() {
                 const updatedExpert = await response.json();
                 setExpert(updatedExpert);
                 setIsModalOpen(false);
+                setEditExpert(null);
             } else {
                 console.error('Failed to update expert profile');
             }
@@ -85,29 +86,29 @@ function ExpertProfile() {
                                 <div className="w-2/3 flex flex-col">
                                     <div className="flex justify-between items-center">
                                         <h2 className="text-3xl mb-3 font-bold text-[#1D3557]">Profile Information</h2>
-                                        <button className="bg-[#BABABA] font-bold mr-3 text-[#1D3557] px-4 py-1 rounded-full" onClick={handleEditClick}>
+                                        <button className="bg-[#BABABA] font-bold mr-3 text-[#1D3557] px-4 py-1 rounded-full hover:bg-gray-200" onClick={handleEditClick}>
                                             Edit
                                         </button>
                                     </div>
                                     <div className="space-y-2">
                                         <div className="grid grid-cols-2 gap-x-4 gap-y-2">
-                                            <div className="text-xl font-bold text-dark">Name:</div>
-                                            <div className="text-xl font-bold text-[#D8CFD0]">{expert.name}</div>
+                                            <div className="text-xl font-medium text-dark">Name:</div>
+                                            <div className="text-xl font-medium text-[#D8CFD0]">{expert.name}</div>
 
-                                            <div className="text-xl font-bold text-dark">Email:</div>
-                                            <div className="text-xl font-bold text-[#D8CFD0]">{expert.email}</div>
+                                            <div className="text-xl font-medium text-dark">Email:</div>
+                                            <div className="text-xl font-medium text-[#D8CFD0]">{expert.email}</div>
 
-                                            <div className="text-xl font-bold text-dark">Phone:</div>
-                                            <div className="text-xl font-bold text-[#D8CFD0]">{expert.phoneNumber}</div>
+                                            <div className="text-xl font-medium text-dark">Phone:</div>
+                                            <div className="text-xl font-medium text-[#D8CFD0]">{expert.phoneNumber}</div>
 
-                                            <div className="text-xl font-bold text-dark">Age:</div>
-                                            <div className="text-xl font-bold text-[#D8CFD0]">{expert.age}</div>
+                                            <div className="text-xl font-medium text-dark">Age:</div>
+                                            <div className="text-xl font-medium text-[#D8CFD0]">{expert.age}</div>
 
-                                            <div className="text-xl font-bold text-dark">Occupation:</div>
-                                            <div className="text-xl font-bold text-[#D8CFD0]">{expert.occupation}</div>
+                                            <div className="text-xl font-medium text-dark">Occupation:</div>
+                                            <div className="text-xl font-medium text-[#D8CFD0]">{expert.occupation}</div>
 
-                                            <div className="text-xl font-bold text-dark">Area of Expertise:</div>
-                                            <div className="text-xl font-bold text-[#D8CFD0]">{expert.expertise}</div>
+                                            <div className="text-xl font-medium text-dark">Area of Expertise:</div>
+                                            <div className="text-xl font-medium text-[#D8CFD0]">{expert.expertise}</div>
                                         </div>
                                     </div>
                                 </div>
@@ -132,85 +133,90 @@ function ExpertProfile() {
 
             {/* Edit Profile Modal */}
             {editExpert && (
-                <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-                    <h2 className="text-xl flex justify-center text-[#D8CFD0] font-bold mb-4">Edit Profile</h2>
-                    <div className="space-y-4">
-                        <div>
-                            <label className="block text-xl font-medium text-gray-700">Name</label>
-                            <input
-                                placeholder='eg. John Doe'
-                                type="text"
-                                value={editExpert.name}
-                                onChange={(e) => setEditExpert({ ...editExpert, name: e.target.value })}
-                                className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-xl font-medium text-gray-700">Email</label>
-                            <input
-                                type="email"
-                                placeholder='eg. abc@gmail.com'
-                                value={editExpert.email}
-                                onChange={(e) => setEditExpert({ ...editExpert, email: e.target.value })}
-                                className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-xl font-medium text-gray-700">Phone</label>
-                            <input
-                                type="text"
-                                placeholder='eg. 123-456-7890'
-                                value={editExpert.phoneNumber}
-                                onChange={(e) => setEditExpert({ ...editExpert, phoneNumber: e.target.value })}
-                                className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-xl font-medium text-gray-700">Age</label>
-                            <input
-                                type="text"
-                                placeholder='eg. 25'
-                                value={editExpert.age}
-                                onChange={(e) => setEditExpert({ ...editExpert, age: e.target.value })}
-                                className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-xl font-medium text-gray-700">Occupation</label>
-                            <input
-                                type="text"
-                                placeholder='eg. Doctor/Engineer'
-                                value={editExpert.occupation}
-                                onChange={(e) => setEditExpert({ ...editExpert, occupation: e.target.value })}
-                                className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-xl font-medium text-gray-700">Area of Expertise</label>
-                            <input
-                                type="text"
-                                placeholder='eg. Financial Management'
-                                value={editExpert.expertise}
-                                onChange={(e) => setEditExpert({ ...editExpert, expertise: e.target.value })}
-                                className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
-                            />
-                        </div>
-                        <div className="flex justify-center space-x-2">
-                            <button
-                                className="bg-gray-300 text-gray-700 text-xl font-bold px-4 py-2 rounded-md"
-                                onClick={() => setIsModalOpen(false)}
-                            >
-                                Cancel
-                            </button>
-                            <button
-                                className="bg-[#D8CFD0] text-[#817B7B] text-xl font-bold px-4 py-2 rounded-md"
-                                onClick={handleSaveClick}
-                            >
-                                Save
-                            </button>
+                <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+                    <div className="bg-[#697184] p-6 rounded-md shadow-lg max-w-3xl w-full">
+                        <h2 className="text-2xl flex justify-center text-[#D8CFD0] font-bold mb-4">Edit Profile</h2>
+                        <div className="space-y-4">
+                            <div>
+                                <label className="block text-lg font-medium text-gray-800">Name</label>
+                                <input
+                                    placeholder='eg. John Doe'
+                                    type="text"
+                                    value={editExpert.name}
+                                    onChange={(e) => setEditExpert({ ...editExpert, name: e.target.value })}
+                                    className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-lg font-medium text-gray-800">Email</label>
+                                <input
+                                    type="email"
+                                    placeholder='eg. abc@gmail.com'
+                                    value={editExpert.email}
+                                    onChange={(e) => setEditExpert({ ...editExpert, email: e.target.value })}
+                                    className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-lg font-medium text-gray-800">Phone</label>
+                                <input
+                                    type="text"
+                                    placeholder='eg. 123-456-7890'
+                                    value={editExpert.phoneNumber}
+                                    onChange={(e) => setEditExpert({ ...editExpert, phoneNumber: e.target.value })}
+                                    className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-lg font-medium text-gray-800">Age</label>
+                                <input
+                                    type="text"
+                                    placeholder='eg. 25'
+                                    value={editExpert.age}
+                                    onChange={(e) => setEditExpert({ ...editExpert, age: e.target.value })}
+                                    className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-lg font-medium text-gray-800">Occupation</label>
+                                <input
+                                    type="text"
+                                    placeholder='eg. Doctor/Engineer'
+                                    value={editExpert.occupation}
+                                    onChange={(e) => setEditExpert({ ...editExpert, occupation: e.target.value })}
+                                    className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-lg font-medium text-gray-800">Area of Expertise</label>
+                                <input
+                                    type="text"
+                                    placeholder='eg. Financial Management'
+                                    value={editExpert.expertise}
+                                    onChange={(e) => setEditExpert({ ...editExpert, expertise: e.target.value })}
+                                    className="mt-1 block w-full bg-[#D8CFD0] rounded-md shadow-sm p-2 focus:outline-none focus:border-none"
+                                />
+                            </div>
+                            <div className="flex justify-center space-x-2">
+                                <button
+                                    className="bg-gray-300 text-gray-700 font-bold px-4 py-2 rounded-md hover:bg-gray-400 transition-colors"
+                                    onClick={() => {
+                                        setIsModalOpen(false);
+                                        setEditExpert(null);
+                                    }}
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    className="bg-[#4A4A4A] text-white font-bold px-4 py-2 rounded-md hover:bg-[#4A4A4A]/70 transition-colors"
+                                    onClick={handleSaveClick}
+                                >
+                                    Save
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </Modal>
+                </div>
             )}
         </div>
     );
